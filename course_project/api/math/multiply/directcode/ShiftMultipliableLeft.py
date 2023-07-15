34a3d871a51d88c454f131fab386757cf70474be
albert-atabekyan <albert.atabekyan1200@gmail.com>
2023-07-15 21:36:26 +0300

    Рефакторинг: такое имя лучше отражает смысл структуры

--- course_project/api/math/multiply/directcode/ShiftMultipliableLeft.py
+++ course_project/api/math/multiply/directcode/ShiftMultipliableLeft.py
@@ -4,13 +4,13 @@ from course_project.api.math.multiply.Multiply import Multiply
 
 class ShiftMultipliableLeft(Multiply):
     def multiply(self, multipliable: BinaryCode, multiplier: BinaryCode):
-        data = list()
+        right_sequence = list()
 
         multiplier_binary = multiplier.get_direct_code()
         result = BinaryCode(0, 8)
 
-        data.append(result.get_direct_code())
-        data.append(multipliable.get_direct_code())
+        right_sequence.append(result.get_direct_code())
+        right_sequence.append(multipliable.get_direct_code())
 
         for index in range(multipliable.bitness - 1, 0, -1):
             bit = multiplier_binary[index]
@@ -18,11 +18,11 @@ class ShiftMultipliableLeft(Multiply):
             if bit == '1':
                 result += multipliable
 
-            data.append(result.get_direct_code())
+            right_sequence.append(result.get_direct_code())
 
             if index != 0:
                 multipliable = multipliable << 1
-                data.append(multipliable.get_direct_code())
-        data.append(result.get_direct_code())
+                right_sequence.append(multipliable.get_direct_code())
+        right_sequence.append(result.get_direct_code())
 
-        print(data)
+        print(right_sequence)
